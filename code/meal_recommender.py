d96b7945a0753a410e6f985b56f4f0fc7d4283a7
Vansh Nagpal <2002vansh@gmail.com>
2024-02-22 14:30:38 -0500

    prototype evaluation metrics

--- code/meal_recommender.py
+++ code/meal_recommender.py
@@ -1,3 +1,5 @@
+# TODO document all methods inputs/outupts
+
 import random
 import json
 from meal_config import MealConfig
@@ -9,7 +11,7 @@ class MealRecommender:
     def __init__(self, user):
         self.user = user
         self.recipe_set, self.beverage_set, self.user_request = self.ReadInputs()
-
+        self.recommendation = None
 
     def ReadInputs(self):
         # Read R3 dataset
@@ -31,8 +33,6 @@ class MealRecommender:
 
     def RunMealRecStrat(self):
         # Runs Meal Recommendation Strategy to create personalized meal plan
-        
-        amount_meals = self.user_request['recommendations_per_day'] # integer
         rec_constraints = self.user_request['recommendation_constraints'] # [{'meal': 'breakfast'/'lunch'/'dinner', 'time': 'HH:MM'}']
         amount_days = self.user_request['time_period']
         
@@ -63,25 +63,99 @@ class MealRecommender:
                 
                 # Random Method for Recommendation
                 if meal_info.meal_config.has_beverage():
-                    meal['beverage'] = random.choice(list(self.beverage_set.keys()))
+                    meal['Beverage'] = random.choice(list(self.beverage_set.keys()))
                 if meal_info.meal_config.has_main_course():
-                    meal['main_course'] = random.choice(list(self.recipe_set.keys()))
+                    meal["Main Course"] = random.choice(list(self.recipe_set.keys()))
                 if meal_info.meal_config.has_dessert():
-                    meal['dessert'] = random.choice(list(self.recipe_set.keys()))
+                    meal['Dessert'] = random.choice(list(self.recipe_set.keys()))
                 if meal_info.meal_config.has_side():
-                    meal['side'] = random.choice(list(self.recipe_set.keys()))
+                    meal['Side'] = random.choice(list(self.recipe_set.keys()))
 
                 meals.append(meal)
             
             meals = {f'day {j + 1}': meals}
             meal_plan.append(meals)        
         
+        self.recommendation = meal_plan
         # return meal plan
         return meal_plan
 
 
-    def WriteMealRecs(self, meal_plan):
+    def WriteMealRecs(self, meal_plan=None):
+        if not meal_plan:
+            meal_plan = self.recommendation
+        
         # Write meal plan to json
-        self.user
         with open(f'../recommendations/recommendation_{self.user}', 'w') as file:
             json.dump(meal_plan, file)
+   
+
+    def EvaluateMealRec(self, meal_plan=None):
+        if not meal_plan:
+            meal_plan = self.recommendation
+
+        
+        def ConfigScore(meal_plan_):
+          score = 0
+          good = len(meal_plan_) == self.user_request['time_period']
+          if good:
+              score += 1
+
+          for i, out_day in enumerate(meal_plan_, 1):
+              # Get each day's recommendations
+              out_day = out_day[f'day {i}']
+              
+              for inp, out in zip(self.user_request['recommendation_constraints'], out_day):
+                  inp = inp['meal_type']
+                  
+                  good &= inp['meal_name'] == out['meal_name']
+                  if good:
+                      score += 1
+                  
+                  good &= inp['time'] == out['meal_time']
+                  if good:
+                      score += 1
+
+                  out.pop('meal_name')
+                  out.pop('meal_time')
+
+                  good &= set(inp['meal_config']) == set(list(out.keys()))
+                  if good :
+                      score += 1
+          return score
+        
+        # Checks if Meal Config is satisfied
+        config_quality_score = ConfigScore(meal_plan)
+        
+
+        def NumDuplicates(meal_plan_):
+            violations = 0
+            for i, day_plan in enumerate(meal_plan_, 1):
+                day_plan = day_plan[f'day {i}']
+                food_items = set()
+                bev_items = set()
+
+                for meal in day_plan:
+                    
+                    if 'Beverage' in meal:
+                        if meal['Beverage'] in bev_items:
+                            violations += 1
+                        bev_items.add(meal['Beverage'])
+                    for food_key in ['Main Course', 'Dessert', 'Side']:
+                        if food_key in meal:
+                            if meal[food_key] in food_items:
+                                violations += 1
+                            food_items.add(meal[food_key])
+            return violations
+        
+        # Check for number of duplicates in meal plan
+        # Rule by Vansh Nagpal: same meal item showing up twice in same day is considered a duplicate
+        violations = NumDuplicates(meal_plan)
+
+        # TODO Calculate violations based on calories and macronutrients
+
+        # TODO Calculate violations based on allergens
+
+        # TODO compute total score
+        return None
+     
