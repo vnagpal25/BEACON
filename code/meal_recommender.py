1293095964e96818eb8561efa9153fa3aa1d8194
Vansh Nagpal <2002vansh@gmail.com>
2024-03-27 01:58:10 -0400

    reader function reads from items data directory

--- code/meal_recommender.py
+++ code/meal_recommender.py
@@ -14,11 +14,16 @@ class MealRecommender:
         self.recommendation = None
 
     def ReadInputs(self):
-        # Read R3 dataset
-        with open('../items_data/recipe_repn.json', 'r') as file:
+        # Read Taco Bell R3
+        with open('../items_data/taco_bell.json', 'r') as file:
             recipes = json.load(file)
             recipes = recipes['recipe-ids']   
         
+        # Read R3 dataset
+        with open('../items_data/recipe_repn.json', 'r') as file:
+            file = json.load(file)
+            recipes = recipes | recipes
+
         # Read Beverages dataset
         with open('../items_data/beverages.json', 'r') as file:
             beverages = json.load(file)
@@ -91,15 +96,20 @@ class MealRecommender:
    
 
     def EvaluateMealRec(self, meal_plan=None):
+        # TODO, make evaluation metrics more configurable, not hard coded
+        # i.e., user may want redundancy, so shouldn't be penalized
+
         if not meal_plan:
             meal_plan = self.recommendation
 
         
         def ConfigScore(meal_plan_):
           score = 0
+          total_possible_score = 0
           good = len(meal_plan_) == self.user_request['time_period']
           if good:
               score += 1
+          total_possible_score += 1
 
           for i, out_day in enumerate(meal_plan_, 1):
               # Get each day's recommendations
@@ -111,10 +121,12 @@ class MealRecommender:
                   good &= inp['meal_name'] == out['meal_name']
                   if good:
                       score += 1
-                  
+                  total_possible_score += 1
+
                   good &= inp['time'] == out['meal_time']
                   if good:
                       score += 1
+                  total_possible_score += 1
 
                   out.pop('meal_name')
                   out.pop('meal_time')
@@ -122,14 +134,17 @@ class MealRecommender:
                   good &= set(inp['meal_config']) == set(list(out.keys()))
                   if good :
                       score += 1
-          return score
+                  total_possible_score += 1
+
+          return score / total_possible_score
         
-        # Checks if Meal Config is satisfied
+        # Checks if Meal Config is satisfied, a number between 0 and 1
         config_quality_score = ConfigScore(meal_plan)
         
 
         def NumDuplicates(meal_plan_):
             violations = 0
+            total_chances = 0
             for i, day_plan in enumerate(meal_plan_, 1):
                 day_plan = day_plan[f'day {i}']
                 food_items = set()
@@ -140,22 +155,25 @@ class MealRecommender:
                     if 'Beverage' in meal:
                         if meal['Beverage'] in bev_items:
                             violations += 1
+                        total_chances += 1
                         bev_items.add(meal['Beverage'])
                     for food_key in ['Main Course', 'Dessert', 'Side']:
                         if food_key in meal:
                             if meal[food_key] in food_items:
                                 violations += 1
+                            total_chances += 1 
                             food_items.add(meal[food_key])
-            return violations
+            return violations / total_chances
         
         # Check for number of duplicates in meal plan
         # Rule by Vansh Nagpal: same meal item showing up twice in same day is considered a duplicate
+        # between 0 and 1
         violations = NumDuplicates(meal_plan)
 
         # TODO Calculate violations based on calories and macronutrients
 
         # TODO Calculate violations based on allergens
 
-        # TODO compute total score
-        return None
+        # TODO discuss with group if this makes sense
+        return (config_quality_score - violations + 1) / 2
      
